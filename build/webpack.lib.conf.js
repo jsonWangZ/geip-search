c246c93e4ec1f635852bfa6b2537990e84605ef2
alex <wangz-an@glodon.com>
2021-07-06 13:24:58 +0800

    feat:测试。

--- build/webpack.lib.conf.js
+++ build/webpack.lib.conf.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   entry: './src/plugins/index.js',
   output: {
-    path: path.resolve(__dirname, '../../lib'),
+    path: path.resolve(__dirname, '../lib'),
     filename: 'Search.js',
     library: 'Search',
     libraryTarget: 'umd',
